5d0bc6181722b736b05512a540d597aed7df95d5
Rir-i <107610241+Rir-i@users.noreply.github.com>
2022-11-11 10:35:08 -0600

    Program_Page_264 first attempt

--- main.cpp
+++ main.cpp
@@ -1,2 +1,41 @@
-// write your code here
+// Rita Chukwunyere osi J00967039
+//  main.cpp
+//  264
+//  Created by Rita Osi on 11/11/22.
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+      int num;
+      cin >> num;
+      bool isPrime = true;
+
+      const int prime [] {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31};
+      
+
+      if(num > 1000 || num < 2){
+        cout << "out of bound";
+      }
+      else{
+          for (const int i : prime)
+              if (i != num)
+                  if ((num % i) == 0){
+                      if (isPrime)
+                          cout << "";
+                      cout << i << " ";
+                      isPrime = false;
+                  }
+          if (isPrime)
+              cout << "Number " << num << " is prime!";
+      }
+
+
+    return 0;
+}
+
 
